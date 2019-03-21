4ec9481b3af2ce16bfc4a904e49ba6c10c618013
Vianney <loutre.a+github@gmail.com>
2019-03-21 15:54:41 +0100

    search for a custom test runner

--- pytest_django/compat.py
+++ pytest_django/compat.py
@@ -4,12 +4,11 @@ try:
     from django.test.utils import setup_databases, teardown_databases  # noqa: F401
 except ImportError:
     # In Django prior to 1.11, teardown_databases is only available as a method on DiscoverRunner
-    from django.test.runner import (  # noqa: F401
-        setup_databases,
-        DiscoverRunner as _DiscoverRunner,
-    )
+    from django.conf import settings
+    from django.test.utils import get_runner
+    from functools import partial
 
-    def teardown_databases(db_cfg, verbosity):
-        _DiscoverRunner(verbosity=verbosity, interactive=False).teardown_databases(
-            db_cfg
-        )
+    _runner = get_runner(settings)
+
+    setup_databases = partial(_runner.setup_databases, _runner)
+    teardown_databases = partial(_runner.teardown_databases, _runner)
